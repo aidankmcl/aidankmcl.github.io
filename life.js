00d92432fdba8789e00ec066354130a7ca7347c7
Aidan <aidankmcl@gmail.com>
2019-06-03 00:22:42 +0200

    MVP|

--- life.js
+++ life.js
@@ -1,17 +1,47 @@
-// https://p5js.org/examples/simulate-game-of-life.html
+// Adapted from https://p5js.org/examples/simulate-game-of-life.html
 let w;
+
 let columns;
 let rows;
+
 let board;
 let next;
 
-let to;
-let from;
+let tempColor;
+let toColor;
+let fromColor;
+let xRatio;
+let yRatio;
+
+let colorQuadrants = {
+  xBottom: null,
+  xTop: null,
+  yBottom: null,
+  yTop: null
+}
+
+function setColor(xRatio, yRatio) {
+  tempColor = lerpColor(
+    lerpColor(colorQuadrants.xBottom, colorQuadrants.xTop, xRatio),
+    lerpColor(colorQuadrants.yBottom, colorQuadrants.yTop, yRatio),
+    (xRatio + (1 - yRatio)) / 2
+  )
+
+  fromColor = color(tempColor.levels[0], tempColor.levels[1], tempColor.levels[2], 0);
+  toColor = color(tempColor.levels[0], tempColor.levels[1], tempColor.levels[2]);
+}
 
 function setup() {
-  to = color(65 , 255, 85);
-  from = color(255, 40, 80);
-  frameRate(30);
+  colorQuadrants = {
+    xBottom: color(255, 40, 80), // Red
+    xTop: color(0, 180, 250), // Blue
+    yBottom: color(65, 255, 85), // Green
+    yTop: color(125, 110, 210) // Purple
+  }
+
+  setColor(0, 0);
+
+  frameRate(10);
   createCanvas(window.innerWidth, window.innerHeight);
   w = 15;
   // Calculate columns and rows
@@ -22,7 +52,7 @@ function setup() {
   for (let i = 0; i < columns; i++) {
     board[i] = new Array(rows);
   }
-  // Going to use multiple 2D arrays and swap them
+  // Going toColor use multiple 2D arrays and swap them
   next = new Array(columns);
   for (i = 0; i < columns; i++) {
     next[i] = new Array(rows);
@@ -35,13 +65,18 @@ function draw() {
   generate();
   for ( let i = 0; i < columns;i++) {
     for ( let j = 0; j < rows;j++) {
-      if (board[i][j] == 0) fill(255);
-      else fill(lerpColor(from, to, board[i][j]));
-      stroke(255);
+      if (board[i][j].activation == 0) fill(255);
+      else fill(lerpColor(fromColor, toColor, board[i][j].activation));
+      noStroke();
       rect(i * w, j * w, w, w);
     }
   }
+}
 
+function mouseMoved() {
+  xRatio = mouseX / windowWidth;
+  yRatio = mouseY / windowHeight;
+  setColor(xRatio, yRatio);
 }
 
 // reset board when mouse is pressed
@@ -53,11 +88,8 @@ function mousePressed() {
 function init() {
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
-      // Lining the edges with 0s
-      if (i == 0 || j == 0 || i == columns-1 || j == rows-1) board[i][j] = 0;
-      // Filling the rest randomly
-      else board[i][j] = random();
-      next[i][j] = 0;
+      board[i][j] = { val: floor(random(2)), activation: 0 };
+      next[i][j] = { val: 0, activation: 0 };
     }
   }
 }
@@ -72,18 +104,21 @@ function generate() {
       let neighbors = 0;
       for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
-          neighbors += (board[x+i][y+j] > 0) ? 1 : 0;
+          neighbors += board[x+i][y+j].val;
         }
       }
 
+      board[x][y].activation = Math.max(0, board[x][y].activation - 0.05);
+
       // A little trick to subtract the current cell's state since
       // we added it in the above loop
-      neighbors -= (board[x][y] > 0) ? 1 : 0;
+      neighbors -= board[x][y].val;
       // Rules of Life
-      if      ((board[x][y] > 0) && (neighbors <  2)) next[x][y] = Math.max(0, board[x][y] - 0.5);            // Loneliness
-      else if ((board[x][y] > 0.5) && (neighbors >  3)) next[x][y] = Math.max(0, board[x][y] - 0.25);           // Overpopulation
-      else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1;           // Reproduction
-      else                                             next[x][y] = board[x][y]; // Stasis
+      if ((board[x][y].val == 1) && (neighbors ==  2)) next[x][y] = { val: 1, activation: 1 }
+      else if ((board[x][y].val == 1) && (neighbors <  2)) next[x][y] = { val: 0, activation: board[x][y].activation }           // Loneliness
+      else if ((board[x][y].val == 1) && (neighbors >  3)) next[x][y] = { val: 0, activation: board[x][y].activation }           // Overpopulation
+      else if ((board[x][y].val == 0) && (neighbors == 3)) next[x][y] = { val: 1, activation: 1 }           // Reproduction
+      else                                                 next[x][y] = board[x][y]; // Stasis
     }
   }
 
