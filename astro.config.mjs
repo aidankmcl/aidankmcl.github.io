ac85558edbbb9ce35c931c76924a45ef90924eca
Aidan <aidankmcl@gmail.com>
2026-03-23 20:36:49 -0500

    change handling of external links, small copy edits

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,10 +11,16 @@ import solid from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 
 import icon from "astro-icon";
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aidan.works",
+  site: "https://littlefire.works",
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+    ]
+  },
   integrations: [UnoCSS({
     injectReset: true,
     configFile: resolve(__dirname, "./src/styles/unocss-config.ts")
